7d213de8259f870dbf7ca825347d036f5433470e
Ymow Wu <kenshin0812@hotmail.com>
2015-01-08 03:49:45 +0800

    account error

---
+++ app/src/main/java/com/materialnavigationdrawer/test11/FragmentButton.java
@@ -0,0 +1,27 @@
+package com.materialnavigationdrawer.test11;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * Created by neokree on 31/12/14.
+ */
+public class FragmentButton extends Fragment{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Button button = new Button(this.getActivity());
+        button.setText("Click Me");
+        button.setGravity(Gravity.CENTER);
+        return button;
+
+    }
+}
